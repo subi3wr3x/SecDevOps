ba3fc34d6e78080ea1e2e17218f225a1c26777d6
JohnO <32470508+modulusmath@users.noreply.github.com>
2017-12-08 20:37:37 -0500

    Add files via upload

---
+++ kdc2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import time
+import xlsxwriter                                                                
+import numpy as np                                                                
+import pandas as pd                                                                
+
+log_dir='/tmp/kdc-deco/'                                                                
+log_file='kdc_all.txt'                                                                
+timestr = time.strftime("%Y%m%d-%H%M%S")                                                                
+df = pd.read_csv(log_dir + log_file,sep='|',names=['Time','KDC','IP','Host','ClntPrinc','SrvPrinc'])                                                                        
+df = df[df['ClntPrinc'].str.contains('crazy')==False]                                                                            
+df = df[df['Host'].str.contains('kdc')==False]                                                                                
+df.set_index('Host',inplace=True)                                                                                        
+df.groupby(['Host', 'ClntPrinc', 'SrvPrinc']).SrvPrinc.agg('count').to_excel('/tmp/kdc_no_spot_agg_' + timestr + '.xlsx','kdc-deco',engine='xlsxwriter')                                                                                            
