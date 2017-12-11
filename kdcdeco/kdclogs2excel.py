0b1131ad408a2d5d64bd9946c66fe58b8db1789b
JohnO <32470508+modulusmath@users.noreply.github.com>
2017-12-11 18:05:20 -0500

    Update kdclogs2excel.py

--- kdcdeco/kdclogs2excel.py
+++ kdcdeco/kdclogs2excel.py
@@ -11,7 +11,8 @@ import pandas as pd
 log_dir='/tmp/kdc-deco/'                                                                
 log_file='kdc_all.txt'                                                                
 timestr = time.strftime("%Y%m%d-%H%M%S")                                                                
-df = pd.read_csv(log_dir + log_file,sep='|',names=['Time','KDC','IP','Host','ClntPrinc','SrvPrinc'])                                                                        
+df = pd.read_csv(log_dir + log_file,sep='|',names=['Time','KDC','IP','Host','ClntPrinc','SrvPrinc'])  
+#Filter if you need to...
 df = df[df['ClntPrinc'].str.contains('crazy')==False]                                                                            
 df = df[df['Host'].str.contains('kdc')==False]                                                                                
 df.set_index('Host',inplace=True)                                                                                        
