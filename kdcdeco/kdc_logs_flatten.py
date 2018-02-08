5b2fed2cf9a24730fe85892e515e9af39a26fb3f
JohnO <32470508+modulusmath@users.noreply.github.com>
2018-02-07 21:52:26 -0500

    Add files via upload

--- kdcdeco/kdc_logs_flatten.py
+++ kdcdeco/kdc_logs_flatten.py
@@ -4,37 +4,46 @@
 """ we analyze the logs files to see who is still hitting the old boxes.   """
 
 """
-This:
+This log file:
 
 Feb 26 17:02:01 krb5.example.com krb5kdc[26922](info): AS_REQ (7 etypes {23 -133 -128 3 1 24 -135}) 192.168.1.3: \
 ISSUE: authtime 1235664121, etypes {rep=23 tkt=16 ses=23}, kprutser@EXAMPLE.COM for krbtgt/EXAMPLE.COM@EXAMPLE.COM
 
-becomes this:
+becomes this csv formatted line:
 
+| Time          |     KDC        |CLient IP  |Client hostname        |Client Principal    |Service Principal             |    
 |Feb 26 17:02:01|krb5.example.com|192.168.1.3|hostname_of_192.168.1.3|kprutser@EXAMPLE.COM|krbtgt/EXAMPLE.COM@EXAMPLE.COM|
 
-for easy shuffling into Pandas 
+for easy shuffling into Pandas
+ 
 """
 
 import os                                                        
 import re                                                        
-import sys                                                        
+import sys
 import glob                                                        
 import socket                                                        
-                                                    
-                                                     
 
-sep='|'                                                                                
-#Modify your KDC name pattern and 'REALM' as needed:
-data_pat = re.compile("(2017-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]).*\
-?([a-z][a-z]ikdc0[0-9]).*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):.*?([a-zA-Z0-9_\.\/\-]\
-{2,50}@[a-zA-Z0-9\.]{2,20})\s+for\s+([a-zA-Z0-9_\.\/\-]{2,50}@REALM)")                                                        
-kdc_csv="/tmp/kdc_all.txt"                                                                
-
-open(kdc_csv,'w').close                                                                
-#Put your logs in here to analyze """
-os.chdir("/tmp/kdc-deco/")                                                                        
-
+sep='|'
+year="2018"
+time_pat=(year + "-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.*?")
+realm="example.com"
+kdc_pat="[a-z0-9]{1,99}." + realm
+IP_pat="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+clprinc_pat="[a-zA-Z0-9_\.\/\-]{2,99}@" + realm
+sprinc_pat="[a-zA-Z0-9_\.\/\-]{2,99}@" + realm
+kdc_log_file_glob="mit_kdc_log*"                                                                
+kdc_csv="/tmp/kdc_all.txt"
+data_pat = re.compile( (time_pat) 
+                       + "\s+"                           \
+                       + (kdc_pat)                       \
+                       + "\s+krb5kdc\[[0-9]{1,10}\].*?"  \
+                       + (IP_pat)                        \
+                       + ":.*?"                          \
+                       + (clprinc_pat)                   \
+                       + "\s+for\s+"                     \
+                       + (sprinc_pat) )
+                
 def lookup(x):                                                                                
     try:                                                                                        
         hn = socket.gethostbyaddr(x)[0]                                                                                        
@@ -43,25 +52,50 @@ def lookup(x):
         hn = 'nohost'                                                                                                        
     return hn                                                                                                        
 
+os.chdir(".")                                                                        
 seen={}     
-#Modify your file name pattern as needed
-for kdclog in glob.glob("kdc-*"):                                                                                                                
-    with open(kdclog) as file:                                                                                                                        
-        for line in file:                                                                                                                                
+if glob.glob(kdc_log_file_glob):
+    #Zero out the destination if the source log files exist - everytime
+    open(kdc_csv,'w').close                                                                                              
+    for kdclog in glob.glob(kdc_log_file_glob):             
+        for line in open(kdclog):                                                                                                                                
             data_is_valid  = data_pat.search(line)                                                                                                                                
-        if data_is_valid:                                                                                                                                        
-            time    = data_is_valid.group(1) 
-            kdc     = data_is_valid.group(2)                                                                                                                                                
-            IP      = data_is_valid.group(3)                                                                                                                                                        
-            cprinc  = data_is_valid.group(4)                                                                                                                                                                
-            sprnc   = data_is_valid.group(5)                                                                                                                                                                        
-        if IP not in seen:                                                                                                                                                                                
-            print (IP,': No')                                                                                                                                                                                
-            hn=lookup(IP)                                                                                                                                                                                        
-            seen[IP]=hn                                                                                                                                                                                                
-        else:                                                                                                                                                                                                        
-            hn=seen[IP]                                                                                                                                                                                                        
-    string=time + sep + kdc + sep + IP  + sep + hn + sep + cprinc + sep + sprnc + "\n"                                                                                                                                                                                                                                        
-    with open(kdc_csv,'a') as mfyi:                                                                                                                                                                                                                                        
-        mfyi.write(string)                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                       
+            if data_is_valid:      
+                print ("OK:" + line)           
+                print ("0 " + data_is_valid.group(0))  
+                print ("obj",data_is_valid)
+                print ("groups",data_is_valid.groups())                                                                                                  
+                try:
+                    time    = data_is_valid.group(1) 
+                    kdc     = data_is_valid.group(2)                                                                                                                                                
+                    IP      = data_is_valid.group(3)                                                                                                                                                        
+                    cprinc  = data_is_valid.group(4)                                                                                                                                                                
+                    sprnc   = data_is_valid.group(5)
+                except IndexError as e:
+                    print("Bailing: no sub matches:",e)
+                    sys.exit()                                                                                                                                                                        
+                if IP not in seen:                                                                                                                                                                                
+                        print (IP,': No')                                                                                                                                                                                
+                        hn = lookup(IP)                                                                                                                                                                                        
+                        seen[IP] = hn                                                                                                                                                                                                
+                else:                                                                                                                                                                                                        
+                    hn = seen[IP]                                                                                                                                                                                                        
+                    string = time  \
+                          + sep    \
+                          + kdc    \
+                          + sep    \
+                          + IP     \
+                          + sep    \
+                          + hn     \
+                          + sep    \
+                          + cprinc \
+                          + sep    \
+                          + sprnc  \
+                          + "\n"                                                                                                                                                                                                                                        
+                    with open(kdc_csv,'a') as myfile:                                                                                                                                                                                                                                        
+                        myfile.write(string)
+            else:
+                    print('No matches')                                                                                                                                                                                                                                        
+else:
+    print("No files found matching " + kdc_log_file_glob)
+    
