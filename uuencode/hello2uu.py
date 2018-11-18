954e928940789c0b1ef8b15819fd7cb0a83aafb1
JohnO <32470508+jouellnyc@users.noreply.github.com>
2018-11-18 12:56:05 -0500

    Add files via upload

---
+++ uuencode/hello2uu.py
@@ -0,0 +1,36 @@
+#!/home/john/anaconda3/bin/python3
+
+""" Given a  file encoded with uu2hello.py, reverse it """
+
+import sys
+if len(sys.argv) < 2:
+    print(sys.argv[0],"file")
+    sys.exit
+else:
+    myfile=sys.argv[1]
+    myfileout=sys.argv[1] + ".uu"
+
+def main():
+    try:
+        with open(myfile) as fh:
+            with open(myfileout,'w') as fh2:
+                all_lines=fh.readlines()
+                for line in all_lines:
+                    line=line\
+                    .replace('HELLO000','`').replace('HELLO001','~').replace('HELLO002','!')\
+                    .replace('HELLO003','@').replace('HELLO04','#').replace('HELLO05','$')\
+                    .replace('HELLO06','%').replace('HELLO07','^').replace('HELLO08','&')\
+                    .replace('HELLO09','*').replace('HELLO010','(').replace('HELLO011',')')\
+                    .replace('HELLO012','_').replace('HELLO013','-').replace('HELLO014','+')\
+                    .replace('HELLO015','=').replace('HELLO016','{').replace('HELLO017','}')\
+                    .replace('HELLO018','[').replace('HELLO019',']').replace('HELLO020','|')\
+                    .replace('HELLO021','\\').replace('HELLO022',':').replace('HELLO023',';')\
+                    .replace("HELLO024","'").replace('HELLO025','<').replace('HELLO026',',')\
+                    .replace('HELLO027','>').replace('HELLO028','.').replace('HELLO029','?')\
+                    .replace('HELLO030','/').replace('HELLO31','"')
+                    fh2.write(line)
+    except Exception as e:
+        print("Error:",e)
+
+if __name__ == "__main__":
+    main()
