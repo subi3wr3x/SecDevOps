e0b020690b7e6c7b402b1962cabc337ee69698f7
jouellnyc <jouellnyc@gmail.com>
2018-11-20 11:16:56 -0500

    initial commit; for rename

---
+++ uuencode-py/test_uu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+### Simple Tester / Wrapper ###
+
+set -ue
+
+UUFILE=$1
+HELLOFILE="${UUFILE}".hello
+
+sha256sum $UUFILE
+./uu2hello.py $UUFILE
+./hello2uu.py $HELLOFILE
+sha256sum "$HELLOFILE".uue
