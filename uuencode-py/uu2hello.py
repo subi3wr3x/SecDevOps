6f9c459052fd615eb8168816333eaed280eaad14
jouellnyc <jouellnyc@gmail.com>
2018-11-23 23:02:15 -0500

    add def

--- uuencode-py/uu2hello.py
+++ uuencode-py/uu2hello.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+""" Give uuencoded filed, encode each special character with a HELLO variant """ 
+
 import sys
 if len(sys.argv) < 2:
     print(sys.argv[0],"file")
@@ -8,28 +10,32 @@ else:
     myfile=sys.argv[1]
     myfileout=sys.argv[1] + ".hello"
 
-try:
-    with open(myfile) as fh:
-            all_lines=fh.readlines()
-            all_lines = all_lines[:-1] # Remove Final 'end' line
-            all_lines = all_lines[:-1] # Remove second to last  '`' line
-            all_lines = all_lines[1:]  # Remove Inital Unix line
-            with open(myfileout,'w') as fh2:
-                for line in all_lines:
-                    if line.startswith('M'):           #M will not start the line %100 of the time, but close
-                        line = "HELLOKITTY" + line[1:] #Change to suit your needs
-                    line=line\
-                    .replace('`','HELLO000').replace('~','HELLO001').replace('!','HELLO002')\
-                    .replace('@','HELLO003').replace('#','HELLO04').replace('$','HELLO05')\
-                    .replace('%','HELLO06').replace('^','HELLO07').replace('&','HELLO08')\
-                    .replace('*','HELLO09').replace('(','HELLO010').replace(')','HELLO011')\
-                    .replace('_','HELLO012').replace('-','HELLO013').replace('+','HELLO014')\
-                    .replace('=','HELLO015').replace('{','HELLO016').replace('}','HELLO017')\
-                    .replace('[','HELLO018').replace(']','HELLO019').replace('|','HELLO020')\
-                    .replace('\\','HELLO021').replace(':','HELLO022').replace(';','HELLO023')\
-                    .replace("'","HELLO024").replace('<','HELLO025').replace(',','HELLO026')\
-                    .replace('>','HELLO027').replace('.','HELLO028').replace('?','HELLO029')\
-                    .replace('/','HELLO030').replace('"','HELLO31')
-                    fh2.write(line)
-except Exception as e:
-    print("Error:",e)
+def main():
+    try:
+        with open(myfile) as fh:
+                all_lines=fh.readlines()
+                all_lines = all_lines[:-1] # Remove Final 'end' line
+                all_lines = all_lines[:-1] # Remove second to last  '`' line
+                all_lines = all_lines[1:]  # Remove Inital Unix line
+                with open(myfileout,'w') as fh2:
+                    for line in all_lines:
+                        if line.startswith('M'):           #M will not start the line %100 of the time, but close
+                            line = "HELLOKITTY" + line[1:] #Change to suit your needs
+                        line=line\
+                        .replace('`','HELLO000').replace('~','HELLO001').replace('!','HELLO002')\
+                        .replace('@','HELLO003').replace('#','HELLO04').replace('$','HELLO05')\
+                        .replace('%','HELLO06').replace('^','HELLO07').replace('&','HELLO08')\
+                        .replace('*','HELLO09').replace('(','HELLO010').replace(')','HELLO011')\
+                        .replace('_','HELLO012').replace('-','HELLO013').replace('+','HELLO014')\
+                        .replace('=','HELLO015').replace('{','HELLO016').replace('}','HELLO017')\
+                        .replace('[','HELLO018').replace(']','HELLO019').replace('|','HELLO020')\
+                        .replace('\\','HELLO021').replace(':','HELLO022').replace(';','HELLO023')\
+                        .replace("'","HELLO024").replace('<','HELLO025').replace(',','HELLO026')\
+                        .replace('>','HELLO027').replace('.','HELLO028').replace('?','HELLO029')\
+                        .replace('/','HELLO030').replace('"','HELLO31')
+                        fh2.write(line)
+    except Exception as e:
+        print("Error:",e)
+
+if __name__ == "__main__":
+    main()
