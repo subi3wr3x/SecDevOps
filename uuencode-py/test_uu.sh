c64390a6bc779eda2ecd5721e303816c8d7b3e09
jouellnyc <jouellnyc@gmail.com>
2019-04-12 22:11:17 -0400

    Update uuencode

--- uuencode-py/test_uu.sh
+++ uuencode-py/test_uu.sh
@@ -4,12 +4,15 @@
 
 set -ue
 
-UUFILE=$1
-HELLOFILE="${UUFILE}".hello
-UUHELLOFILE="${UUFILE}".hello.uue
-
+FILE=$1
+sha256sum $FILE
+UUFILE="${FILE}".uue
+uuencode $FILE $FILE > $UUFILE
 sha256sum $UUFILE
+
+HELLOFILE="${UUFILE}".hello
 ./uu2hello.py $UUFILE
+UUHELLOFILE="${UUFILE}".hello.uue
 ./hello2uu.py $HELLOFILE
 sha256sum $UUHELLOFILE 
 diff $UUFILE $UUHELLOFILE
